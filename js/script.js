4d4880f6d50a84c658285672eb9bde667294cb24
ermatov <abdulmalik>
2023-12-26 16:26:02 +0500

    ass

---
+++ js/script.js
@@ -0,0 +1,9 @@
+window.addEventListener("DOMContentLoaded", () => {
+  const menu = require('../modules/menu'),
+    aboutSlider = require('../modules/aboutSlider'),
+    indexSlider = require('../modules/indexSlider')
+
+    menu ()
+    aboutSlider ()
+    indexSlider ()
+})
